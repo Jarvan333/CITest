c95ff1cb8c38110e24f37515f50faae7f871941a
Jarvan333 <developer333@hotmail.com>
2019-05-28 16:57:07 +0800

    test service

--- src/Services/OrderService/Controllers/ValuesController.cs
+++ src/Services/OrderService/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace OrderService.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "orderValue", "value2" };
         }
 
         // GET api/values/5
