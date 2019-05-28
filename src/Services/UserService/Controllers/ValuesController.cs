b3b368a3cf66162c309f5f2bd89baf2567f1a93d
Jarvan333 <developer333@hotmail.com>
2019-05-28 17:31:26 +0800

    test jenkins

--- src/Services/UserService/Controllers/ValuesController.cs
+++ src/Services/UserService/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace UserService.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "userValue" };
+            return new string[] { "userValue1" };
         }
 
         // GET api/values/5
