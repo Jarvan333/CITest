1d68275d2013d609c34a0a64c00a76e9db4f2e6c
Jarvan333 <developer333@hotmail.com>
2019-06-18 14:57:00 +0800

    nope

--- src/Services/UserService/Controllers/ValuesController.cs
+++ src/Services/UserService/Controllers/ValuesController.cs
@@ -3,44 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 
-namespace UserService.Controllers
-{
+namespace UserService.Controllers {
     [Route("api/[controller]")]
     [ApiController]
-    public class ValuesController : ControllerBase
-    {
+    public class ValuesController : ControllerBase {
+        private readonly IHostingEnvironment _env;
+
+        public ValuesController(IHostingEnvironment env) {
+            _env = env;
+        }
         // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
-        {
-            return new string[] { "userValue", Dns.GetHostName() };
+        public ActionResult<IEnumerable<string>> Get() {
+            return new string[] { "userValue", Dns.GetHostName(), _env.EnvironmentName };
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
-        {
+        public ActionResult<string> Get(int id) {
             return Program.ArgString;
         }
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody] string value)
-        {
+        public void Post([FromBody] string value) {
         }
 
         // PUT api/values/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
+        public void Put(int id, [FromBody] string value) {
         }
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+        public void Delete(int id) {
         }
     }
 }
